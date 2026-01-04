bf67a7b1169646d9ff86d36ffcf66f6ac82b1275
misProyectosPropios <inakifrutos@outlook.com>
2026-01-04 11:16:21 +0000

    Add test2

--- test/test_chip8.c
+++ test/test_chip8.c
@@ -10,14 +10,22 @@ void tearDown(void) {
     // nothing for now
 }
 
-void firstTests_execute6XNNBehavesCorrectly(void) {
+void tests1_execute6XNNBehavesCorrectly(void) {
     cpu_registers_t cpu_registers;
     executeInstruction(0x60FF, &cpu_registers);
     TEST_ASSERT_MESSAGE( cpu_registers.data_register[0] == 0xFF , "Instruction 6XNN doens't change the value of the register");
 }
 
+void tests2_execute7XNNAddValueNNToRegisterCorrectly(void) {
+    cpu_registers_t cpu_registers;
+    executeInstruction(0x6001, &cpu_registers);
+    executeInstruction(0x7001, &cpu_registers);
+    TEST_ASSERT_MESSAGE( cpu_registers.data_register[0] == 0x02 , "Instruction 6XNN doens't change the value of the register");
+}
+
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(firstTests_execute6XNNBehavesCorrectly);
+    RUN_TEST(tests1_execute6XNNBehavesCorrectly);
+    RUN_TEST(tests2_execute7XNNAddValueNNToRegisterCorrectly);
     return UNITY_END();
 }

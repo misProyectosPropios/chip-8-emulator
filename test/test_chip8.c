fe072beacbdcb7e4fe7580a3042601f8e2b1c192
misProyectosPropios <inakifrutos@outlook.com>
2026-01-04 11:48:49 +0000

    ADd test3

--- test/test_chip8.c
+++ test/test_chip8.c
@@ -23,9 +23,17 @@ void tests2_execute7XNNAddValueNNToRegisterCorrectly(void) {
     TEST_ASSERT_MESSAGE( cpu_registers.data_register[0] == 0x02 , "Instruction 6XNN doens't change the value of the register");
 }
 
+void tests3_execute8XY0MovesValueFromYToX(void) {
+    cpu_registers_t cpu_registers;
+    executeInstruction(0x6001, &cpu_registers);
+    executeInstruction(0x8100, &cpu_registers);
+    TEST_ASSERT_MESSAGE( cpu_registers.data_register[1] == 0x01 , "Instruction 8XY0 doens't change the value of the register");
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(tests1_execute6XNNBehavesCorrectly);
     RUN_TEST(tests2_execute7XNNAddValueNNToRegisterCorrectly);
+    RUN_TEST(tests3_execute8XY0MovesValueFromYToX);
     return UNITY_END();
 }

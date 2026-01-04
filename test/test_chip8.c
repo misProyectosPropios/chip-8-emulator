6c3347a0281ac53f733f8b694272c69a0b5120d7
misProyectosPropios <inakifrutos@outlook.com>
2026-01-04 11:56:04 +0000

    Add test to make an OR between registers

--- test/test_chip8.c
+++ test/test_chip8.c
@@ -20,14 +20,22 @@ void tests2_execute7XNNAddValueNNToRegisterCorrectly(void) {
     cpu_registers_t cpu_registers;
     executeInstruction(0x6001, &cpu_registers);
     executeInstruction(0x7001, &cpu_registers);
-    TEST_ASSERT_MESSAGE( cpu_registers.data_register[0] == 0x02 , "Instruction 6XNN doens't change the value of the register");
+    TEST_ASSERT_MESSAGE( cpu_registers.data_register[0] == 0x02 , "Instruction 7XNN doens't add the value to the register");
 }
 
 void tests3_execute8XY0MovesValueFromYToX(void) {
     cpu_registers_t cpu_registers;
     executeInstruction(0x6001, &cpu_registers);
     executeInstruction(0x8100, &cpu_registers);
-    TEST_ASSERT_MESSAGE( cpu_registers.data_register[1] == 0x01 , "Instruction 8XY0 doens't change the value of the register");
+    TEST_ASSERT_MESSAGE( cpu_registers.data_register[1] == 0x01 , "Instruction 8XY0 doens't move the value of the register_from to the register_to");
+}
+
+void tests4_execute8XY0MovesValueFromYToX(void) {
+    cpu_registers_t cpu_registers;
+    executeInstruction(0x60F0, &cpu_registers);
+    executeInstruction(0x610F, &cpu_registers);
+    executeInstruction(0x8101, &cpu_registers);
+    TEST_ASSERT_MESSAGE( cpu_registers.data_register[1] == 0xFF , "Instruction 8XY0 doens't change the value of the register");
 }
 
 int main(void) {

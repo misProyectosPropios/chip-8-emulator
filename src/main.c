87be90e6f20e72f3c006f749c2684d50b317de74
misProyectosPropios <inakifrutos@outlook.com>
2026-01-05 02:35:05 +0000

    Add code to tests sdl

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
-#include "defines.h"
-#include <stdio.h>
+#include <SDL2/SDL.h>
 
+int main(void)
+{
+    SDL_Init(SDL_INIT_VIDEO);
 
-int main() {
-    printf("Hello World");
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL works!",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        0
+    );
+
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }

89a1cbe88efa8ff79e80f40f0860af0af83a319b
misProyectosPropios <inakifrutos@outlook.com>
2026-01-09 11:39:14 +0000

    Modify log to make it look prettiers

--- src/main.c
+++ src/main.c
@@ -3,6 +3,14 @@
 static FILE *log_file = NULL;
 
 
+void logFile(FILE *log_file, uint16_t opcode, cpu_registers_t *cpu) {
+    fprintf(log_file, "PC=%03X OPCODE=%04X\n", cpu->pc, (uint16_t)opcode);   
+    log_keypad(log_file, cpu);
+    fprintf(log_file, "\n");
+    log_timers(log_file, cpu);
+    fprintf(log_file, "\n");
+}
+
 int main() {
     log_file = fopen("./chip8.log", "w");
     if (!log_file) {
@@ -37,135 +45,13 @@ int main() {
         return 1;
     }
 
-    
-
     while (true) {
-        SDL_Event event;
-        if (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                cleanAll(cpu, &game, EXIT_SUCCESS);
-                break;
-            case SDL_KEYUP:
-                switch (event.key.keysym.scancode) {
-                    case SDL_SCANCODE_1:
-                        storeKeyup(cpu, 0x1);
-                        break;
-                    case SDL_SCANCODE_2:
-                        storeKeyup(cpu, 0x2);
-                        break;
-                    case SDL_SCANCODE_3:
-                        storeKeyup(cpu, 0x3);
-                        break;
-                    case SDL_SCANCODE_4:
-                        storeKeyup(cpu, 0xC);
-                        break;
-                    case SDL_SCANCODE_Q:
-                        storeKeyup(cpu, 0x4);
-                        break;
-                    case SDL_SCANCODE_W:
-                        storeKeyup(cpu, 0x5);
-                        break;
-                    case SDL_SCANCODE_E:
-                        storeKeyup(cpu, 0x6);
-                        break;
-                    case SDL_SCANCODE_R:
-                        storeKeyup(cpu, 0xD);
-                        break;
-                    case SDL_SCANCODE_A:
-                        storeKeyup(cpu, 0x7);
-                        break;
-                    case SDL_SCANCODE_S:
-                        storeKeyup(cpu, 0x8);
-                        break;
-                    case SDL_SCANCODE_D:
-                        storeKeyup(cpu, 0x9);
-                        break;
-                    case SDL_SCANCODE_F:
-                        storeKeyup(cpu, 0xE);
-                        break;
-                    case SDL_SCANCODE_Z:
-                        storeKeyup(cpu, 0xA);
-                        break;
-                    case SDL_SCANCODE_X:
-                        storeKeyup(cpu, 0x0);
-                        break;
-                    case SDL_SCANCODE_C:
-                        storeKeyup(cpu, 0xB);
-                        break;
-                    case SDL_SCANCODE_V:
-                        storeKeyup(cpu, 0xF);
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.scancode) {
-                    case SDL_SCANCODE_ESCAPE:
-                        cleanAll(cpu, &game, EXIT_SUCCESS);
-                        break;
-                    case SDL_SCANCODE_1:
-                        storeKeydown(cpu, 0x1);
-                        break;
-                    case SDL_SCANCODE_2:
-                        storeKeydown(cpu, 0x2);
-                        break;
-                    case SDL_SCANCODE_3:
-                        storeKeydown(cpu, 0x3);
-                        break;
-                    case SDL_SCANCODE_4:
-                        storeKeydown(cpu, 0xC);
-                        break;
-                    case SDL_SCANCODE_Q:
-                        storeKeydown(cpu, 0x4);
-                        break;
-                    case SDL_SCANCODE_W:
-                        storeKeydown(cpu, 0x5);
-                        break;
-                    case SDL_SCANCODE_E:
-                        storeKeydown(cpu, 0x6);
-                        break;
-                    case SDL_SCANCODE_R:
-                        storeKeydown(cpu, 0xD);
-                        break;
-                    case SDL_SCANCODE_A:
-                        storeKeydown(cpu, 0x7);
-                        break;
-                    case SDL_SCANCODE_S:
-                        storeKeydown(cpu, 0x8);
-                        break;
-                    case SDL_SCANCODE_D:
-                        storeKeydown(cpu, 0x9);
-                        break;
-                    case SDL_SCANCODE_F:
-                        storeKeydown(cpu, 0xE);
-                        break;
-                    case SDL_SCANCODE_Z:
-                        storeKeydown(cpu, 0xA);
-                        break;
-                    case SDL_SCANCODE_X:
-                        storeKeydown(cpu, 0x0);
-                        break;
-                    case SDL_SCANCODE_C:
-                        storeKeydown(cpu, 0xB);
-                        break;
-                    case SDL_SCANCODE_V:
-                        storeKeydown(cpu, 0xF);
-                        break;
-                    default:
-                        break;
-                }
-                default:
-                    break;
-            }
-
-            
-        }
+        handleInput(cpu, game);
         int16_t opcode = fetchInstruction(cpu);
-        log_keypad(log_file, cpu);
-        log_timers(log_file, cpu);
-        fprintf(log_file, "PC=%03X OPCODE=%04X\n", cpu->pc, (uint16_t)opcode);
+        logFile(log_file, opcode, cpu);
+        //log_keypad(log_file, cpu);
+        //log_timers(log_file, cpu);
+        //fprintf(log_file, "PC=%03X OPCODE=%04X\n", cpu->pc, (uint16_t)opcode);
         
         executeInstruction(opcode, cpu);
         decrementTimer(cpu);
@@ -179,8 +65,6 @@ int main() {
 
         SDL_Delay(4);
     }
-
-    //cleanAll(cpu, &game, EXIT_SUCCESS);
     return 0;
 }
 
@@ -208,6 +92,128 @@ static void renderChip8Screen(struct Game *game, cpu_registers_t *cpu) {
     
 }
 
+void handleInput(cpu_registers_t* cpu, struct Game game) {
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            cleanAll(cpu, &game, EXIT_SUCCESS);
+            break;
+        case SDL_KEYUP:
+            switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_1:
+                    storeKeyup(cpu, 0x1);
+                    break;
+                case SDL_SCANCODE_2:
+                    storeKeyup(cpu, 0x2);
+                    break;
+                case SDL_SCANCODE_3:
+                    storeKeyup(cpu, 0x3);
+                    break;
+                case SDL_SCANCODE_4:
+                    storeKeyup(cpu, 0xC);
+                    break;
+                case SDL_SCANCODE_Q:
+                    storeKeyup(cpu, 0x4);
+                    break;
+                case SDL_SCANCODE_W:
+                    storeKeyup(cpu, 0x5);
+                    break;
+                case SDL_SCANCODE_E:
+                    storeKeyup(cpu, 0x6);
+                    break;
+                case SDL_SCANCODE_R:
+                    storeKeyup(cpu, 0xD);
+                    break;
+                case SDL_SCANCODE_A:
+                    storeKeyup(cpu, 0x7);
+                    break;
+                case SDL_SCANCODE_S:
+                    storeKeyup(cpu, 0x8);
+                    break;
+                case SDL_SCANCODE_D:
+                    storeKeyup(cpu, 0x9);
+                    break;
+                case SDL_SCANCODE_F:
+                    storeKeyup(cpu, 0xE);
+                    break;
+                case SDL_SCANCODE_Z:
+                    storeKeyup(cpu, 0xA);
+                    break;
+                case SDL_SCANCODE_X:
+                    storeKeyup(cpu, 0x0);
+                    break;
+                case SDL_SCANCODE_C:
+                    storeKeyup(cpu, 0xB);
+                    break;
+                case SDL_SCANCODE_V:
+                    storeKeyup(cpu, 0xF);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_ESCAPE:
+                    cleanAll(cpu, &game, EXIT_SUCCESS);
+                    break;
+                case SDL_SCANCODE_1:
+                    storeKeydown(cpu, 0x1);
+                    break;
+                case SDL_SCANCODE_2:
+                    storeKeydown(cpu, 0x2);
+                    break;
+                case SDL_SCANCODE_3:
+                    storeKeydown(cpu, 0x3);
+                    break;
+                case SDL_SCANCODE_4:
+                    storeKeydown(cpu, 0xC);
+                    break;
+                case SDL_SCANCODE_Q:
+                    storeKeydown(cpu, 0x4);
+                    break;
+                case SDL_SCANCODE_W:
+                    storeKeydown(cpu, 0x5);
+                    break;
+                case SDL_SCANCODE_E:
+                    storeKeydown(cpu, 0x6);
+                    break;
+                case SDL_SCANCODE_R:
+                    storeKeydown(cpu, 0xD);
+                    break;
+                case SDL_SCANCODE_A:
+                    storeKeydown(cpu, 0x7);
+                    break;
+                case SDL_SCANCODE_S:
+                    storeKeydown(cpu, 0x8);
+                    break;
+                case SDL_SCANCODE_D:
+                    storeKeydown(cpu, 0x9);
+                    break;
+                case SDL_SCANCODE_F:
+                    storeKeydown(cpu, 0xE);
+                    break;
+                case SDL_SCANCODE_Z:
+                    storeKeydown(cpu, 0xA);
+                    break;
+                case SDL_SCANCODE_X:
+                    storeKeydown(cpu, 0x0);
+                    break;
+                case SDL_SCANCODE_C:
+                    storeKeydown(cpu, 0xB);
+                    break;
+                case SDL_SCANCODE_V:
+                    storeKeydown(cpu, 0xF);
+                    break;
+                default:
+                    break;
+            }
+            default:
+                break;
+        }            
+    }
+}
 
 
 uint8_t *load_rom(const char *path, size_t *out_size) {
@@ -249,15 +255,15 @@ void print_rom_hex(uint8_t *rom, size_t size) {
 
 
 void log_keypad(FILE *log_file, cpu_registers_t *cpu) {
-    fprintf(log_file, "KEYPAD=");
+    fprintf(log_file, "\tKEYPAD = \n\t");
     for (int i = 0; i < 16; i++) {
         fprintf(log_file, "%X:%d ", i, cpu->keypad[i]);
     }
 }
 
 void log_timers(FILE *log_file, cpu_registers_t *cpu) {
-    fprintf(log_file, "DELAY_TIMER=%d", cpu->delay_timer);
-    fprintf(log_file, "SOUND_TIMER=%d", cpu->sound_timer);
+    fprintf(log_file, "\tDELAY_TIMER=%d", cpu->delay_timer);
+    fprintf(log_file, "\tSOUND_TIMER=%d", cpu->sound_timer);
 }
 
 
